920f6b0ec557b6781d512d0a52b27ab6afaca8ee
Hashmat ibrahimi <hashmatibrahimi@Hashmats-MacBook-Pro.local>
2023-01-28 17:16:27 -0500

    minor clean up

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,14 @@
 import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "@/server/routers/_app";
+import { appRouter, AppRouter } from "@/server/routers/_app";
+import {
+  inferProcedureOutput,
+  inferRouterInputs,
+  inferRouterOutputs,
+} from "@trpc/server";
+
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
+
 // export API handler
 // @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
 import { useState } from "react";
+import { RouterOutput } from "./api/trpc/[trpc]";
+
+const btn =
+  " inline-flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-4";
 
 export default function Home() {
   const [ids, updateIds] = useState(() => getOptionsForVote());
@@ -11,14 +16,18 @@ export default function Home() {
   const firstPokemon = trpc["get-pokemon-by-id"].useQuery({ id: first });
   const secondPokemon = trpc["get-pokemon-by-id"].useQuery({ id: second });
 
-  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+  const voteForRoundest = (selected: number) => {
+    //todo: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-3xl text-center pt-8">Which Pokémon is Rounder</div>
       <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center ">
-        <div className="w-60 h-60 flex flex-col">
+      <div className="border pb-20 rounded flex justify-between max-w-3xl items-center ">
+        <div className="w-60 h-60 flex flex-col items-center">
           <img
             src={firstPokemon.data?.sprites.front_default || "?"}
             className="w-full"
@@ -27,11 +36,14 @@ export default function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
         </div>
         <div className="p-8">
           <p>VS</p>
         </div>
-        <div className="w-60 h-60 flex flex-col ">
+        <div className="w-60 h-60 flex flex-col items-center ">
           <img
             src={secondPokemon.data?.sprites.front_default || "?"}
             alt=""
@@ -40,8 +52,28 @@ export default function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(second)}>
+            Rounder
+          </button>
         </div>
       </div>
     </div>
   );
 }
+
+type PokemonFromServer = RouterOutput["get-pokemon-by-id"];
+
+const PokemonListing: React.FC<{
+  pokemon: PokemonFromServer;
+  vote: () => void;
+}> = (props) => {
+  return (
+    <div className="w-64 h-64 flex flex-col items-center">
+      <img src={props.pokemon.sprites.front_default} className="w-full" />
+      <div>{props.pokemon.name}</div>
+      <button className={btn} onClick={() => props.vote()}>
+        Rounder
+      </button>
+    </div>
+  );
+};
