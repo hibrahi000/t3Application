746acde4fd50eddc79599f51c2fc296cd32a2a81
Hashmat ibrahimi <hashmatibrahimi@Hashmats-MacBook-Pro.local>
2023-01-28 16:36:11 -0500

    basic ui functionality

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,5 @@
 import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "@/server/routers/index";
+import { appRouter } from "@/server/routers/_app";
 // export API handler
 // @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
+import { useState } from "react";
 
 export default function Home() {
-  const hello = trpc.hello.useQuery({ text: "client" });
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className="h-screen w-screen flex flex-col justify-between items-center relative">
-      <div className="text-2xl text-center pt-8">Which Pokémon is Rounder</div>
-      <div className="p-2">
-        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-          <div className="w-16 h-16 bg-red-200"></div>
-          <div className="p-8">
-            <p>{hello.data.greeting}</p>
-            VS
-          </div>
+  const [ids, updateIds] = useState(() => getOptionsForVote());
 
-          <div className="w-16 h-16 bg-red-200"></div>
+  const [first, second] = ids;
+
+  const firstPokemon = trpc["get-pokemon-by-id"].useQuery({ id: first });
+  const secondPokemon = trpc["get-pokemon-by-id"].useQuery({ id: second });
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center relative">
+      <div className="text-3xl text-center pt-8">Which Pokémon is Rounder</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center ">
+        <div className="w-60 h-60 flex flex-col">
+          <img
+            src={firstPokemon.data?.sprites.front_default || "?"}
+            className="w-full"
+            alt=""
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
+        </div>
+        <div className="p-8">
+          <p>VS</p>
+        </div>
+        <div className="w-60 h-60 flex flex-col ">
+          <img
+            src={secondPokemon.data?.sprites.front_default || "?"}
+            alt=""
+            className="w-full"
+          />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
         </div>
       </div>
     </div>
