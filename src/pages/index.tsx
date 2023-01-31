f2197d4d5eb4207546acea5f9505b43b6d60199d
Hashmat ibrahimi <hashmatibrahimi@Hashmats-MacBook-Pro.local>
2023-01-30 19:41:00 -0500

    finished basic setup of reload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
 import { useState } from "react";
 import { RouterOutput } from "./api/trpc/[trpc]";
+import type React from "react";
 
 const btn =
   " inline-flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-4";
@@ -26,36 +27,24 @@ export default function Home() {
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-3xl text-center pt-8">Which Pokémon is Rounder</div>
       <div className="p-2" />
-      <div className="border pb-20 rounded flex justify-between max-w-3xl items-center ">
-        <div className="w-60 h-60 flex flex-col items-center">
-          <img
-            src={firstPokemon.data?.sprites.front_default || "?"}
-            className="w-full"
-            alt=""
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">
-            {firstPokemon.data?.name}
-          </div>
-          <button className={btn} onClick={() => voteForRoundest(first)}>
-            Rounder
-          </button>
-        </div>
-        <div className="p-8">
-          <p>VS</p>
-        </div>
-        <div className="w-60 h-60 flex flex-col items-center ">
-          <img
-            src={secondPokemon.data?.sprites.front_default || "?"}
-            alt=""
-            className="w-full"
-          />
-          <div className="text-xl text-center capitalize mt-[-2rem]">
-            {secondPokemon.data?.name}
-          </div>
-          <button className={btn} onClick={() => voteForRoundest(second)}>
-            Rounder
-          </button>
-        </div>
+      <div className="border pb-20  rounded flex justify-between max-w-3xl items-center ">
+        {!firstPokemon.isLoading &&
+          firstPokemon.data &&
+          !secondPokemon.isLoading &&
+          secondPokemon.data && (
+            <>
+              <PokemonListing
+                pokemon={firstPokemon.data!}
+                vote={() => voteForRoundest(first)}
+              />
+              <div className="p-8 h-10">VS</div>
+              <PokemonListing
+                pokemon={secondPokemon.data!}
+                vote={() => voteForRoundest(second)}
+              />
+            </>
+          )}
+        <div className="p-2"></div>
       </div>
     </div>
   );
@@ -68,8 +57,8 @@ const PokemonListing: React.FC<{
   vote: () => void;
 }> = (props) => {
   return (
-    <div className="w-64 h-64 flex flex-col items-center">
-      <img src={props.pokemon.sprites.front_default} className="w-full" />
+    <div className="flex flex-col items-center">
+      <img src={props.pokemon.sprites.front_default!} className=" w-64 h-64" />
       <div>{props.pokemon.name}</div>
       <button className={btn} onClick={() => props.vote()}>
         Rounder
